c333ec155bb4a63ecb161a94ca06fc9e62729dda
deny <denisson.graca3@gmail.com>
2023-06-20 22:00:46 -0300

    readme

--- public/index.js
+++ public/index.js
@@ -1,15 +1,21 @@
-import bgMain from "./bg-main.jpg"
 import homeCofe from "./home-cofe.svg"
 import svgBars from "./svg-bars.svg"
 import share from "./shareWithYourFriendsSvg.svg"
 import exercise from "./exercise.svg"
+import pro1 from "./pro1.png";
+import pro2 from "./pro2.png";
+import pro3 from "./pro3.png";
+import pro4 from "./pro4.png";
 
 
 export {
-    bgMain,
     homeCofe,
     svgBars,
     share,
-    exercise
+    exercise,
+    pro1,
+    pro2,
+    pro3,
+    pro4
     
 }
