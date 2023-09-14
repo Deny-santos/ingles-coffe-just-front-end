453d44155f6896f0ffac6145b9eace9d85273d49
deny <denisson.graca3@gmail.com>
2023-09-14 01:27:45 -0300

    new words

--- src/components/mainComponents/MainHeader.tsx
+++ src/components/mainComponents/MainHeader.tsx
@@ -37,7 +37,7 @@ const MainHeader = () => {
                         </span>
                     }
                 </span >
-                <span className='text-light-300 text-sm'>{!isEsp ? "actualizado en": "atualizado em"} 05/09/23</span>
+                <span className='text-light-300 text-sm'>{!isEsp ? "actualizado en": "atualizado em"} 12/09/23</span>
         </header>
     )
 }
